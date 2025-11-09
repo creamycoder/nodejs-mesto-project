d608e455d6b981225af03de2090fd87130de12dc
Nikita Kolankov <kolankovnikita@gmail.com>
2025-11-09 20:40:05 +0300

    Создание схем и моделей для пользователей и карточек

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,21 @@
-import express from 'express';
+import express, { Application } from 'express';
 import mongoose from 'mongoose';
 
 const {PORT = 3000} = process.env;
 
-const app = express();
+const app: Application = express();
 
-mongoose.connect('mongodb://localhost:27017/mestodb');
+const connect = async () => {
+  try {
+    await mongoose.connect('mongodb://localhost:27017/mestodb');
+    app.listen(PORT, () => {
+      console.log(`Приложение прослушивает порт ${PORT}`);
+    });
+  } catch (error) {
+    console.error(error);
+  }
+}
 
-app.listen(PORT, () => {
-  console.log(`Приложение прослушивает порт ${PORT}`);
-});
+connect();
+
+
---
+++ src/models/card.ts
@@ -0,0 +1,41 @@
+import mongoose from 'mongoose';
+import user from './user';
+
+interface ICard extends Document {
+  name: string,
+  link: string,
+  owner: mongoose.Schema.Types.ObjectId,
+  likes: Array<mongoose.Types.ObjectId>,
+  createAt: Date
+}
+
+const cardSchema = new mongoose.Schema<ICard>({
+  name: {
+    type: String,
+    minlength: 2,
+    maxlength: 30,
+    required: true
+  },
+  link: {
+    type: String,
+    required: true
+  },
+  owner: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: user,
+    required: true
+  },
+  likes: {
+    type: [mongoose.Types.ObjectId],
+    default: [],
+    required: true,
+  },
+  createAt: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+export default mongoose.model<ICard>('card', cardSchema);
+
+
